e068fcd865110a8cf79422ce6f834fab7abcc894
prashant.kumarjha <jhaprash29@gmail.com>
2022-04-14 14:42:38 +0530

    test

---
+++ script.js
@@ -0,0 +1,60 @@
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+ const txt = document.createElement("button")
+ txt.innerHTML="Delete";
+ txt.style= "background-color:red ;border-radius:5px;padding:10px;color:white;border:1px solid red";
+  span.className = "close";
+  span.style= "margin:0px"
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("mydata").appendChild(li);
+    
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  const txt = document.createElement("button")
+  txt.innerHTML="Delete"
+  txt.style= "background-color:red ;border-radius:5px;padding:10px;color:white;border:1px solid red";
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
